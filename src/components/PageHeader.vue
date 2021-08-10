b778c3e34166cfbfbda755b89789fbe54b20b05e
tomtiao <tomtiao1234@gmail.com>
2021-08-10 15:47:01 +0800

    基本框架样式

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="header-wrapper">
+    <header class="header">
+      <div class="avatar">
+          <img src="../assets/avatar.png" alt="用户头像" class="avatar-img">
+      </div>
+      <div class="links">
+        <a class="link message" href="#">消息</a>
+        <a class="link login" href="/login">登录</a>
+        <a class="link signup" href="/signup">注册</a>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageHeader'
+});
+</script>
+
+<style scoped>
+.header-wrapper {
+  width: 100vw;
+  color: #fffefe;
+  background-color: #656565;
+  font-size: 18px;
+}
+.header {
+  display: flex;
+  justify-content: flex-end;
+  align-content: center;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 1em 0;
+}
+.avatar {
+  width: 1.5em;
+  height: 1.5em;
+  background-color: #ffffff;
+  border-radius: 50%;
+}
+.avatar-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+}
+.links {
+  display: flex;
+  align-items: center;
+}
+.link {
+  margin-left: 2em;
+  vertical-align: middle;
+}
+.link:first-child {
+  margin-left: 1em;
+}
+a {
+  text-decoration: inherit;
+}
+a:link, a:visited, a:hover, a:active {
+  color: inherit;
+}
+</style>
---
+++ src/components/PageNavigation/PageNavigation.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="nav-wrapper">
+    <nav class="nav">
+      <div class="logo-section">
+        <div class="logo">
+          <img src="../../assets/logo.png" alt="团团维权图标" class="logo-img">
+        </div>
+        <div class="title">
+          <h1>团团维权</h1>
+          <h2>TUANTUANWEIQUAN</h2>
+        </div>
+      </div>
+      <PageNavigationList></PageNavigationList>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PageNavigationList from './PageNavigationList.vue';
+
+export default defineComponent({
+  name: 'PageNavigation',
+  components: {
+    PageNavigationList
+  }
+});
+</script>
+
+<style scoped>
+.nav-wrapper {
+  width: 100vw;
+  color: #888888;
+  font-size: 18px;
+}
+.nav {
+  display: flex;
+  align-items: center;
+  max-width: 1300px;
+  margin: 0 auto;
+}
+.logo-section {
+  display: flex;
+}
+.logo {
+  width: 3em;
+  height: 3em;
+}
+.logo-img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+.title {
+  text-align: center;
+}
+h1, h2 {
+  margin: 0;
+}
+h1 {
+  font-size: 25px;
+  letter-spacing: 0.5em;
+  /* 补偿 letter-spacing 添加的额外末尾间隔 https://github.com/w3c/csswg-drafts/issues/1518 */
+  padding-left: 0.5em;
+  color: #6c6c6c;
+}
+h2 {
+  font-size: 10px;
+  color: #989898;
+}
+</style>
