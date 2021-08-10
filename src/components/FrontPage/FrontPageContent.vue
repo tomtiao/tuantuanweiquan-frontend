c87a2816b3277a04bd0261029f70cbbb4f5a9e26
tomtiao <tomtiao1234@gmail.com>
2021-08-10 16:05:07 +0800

    init

---
+++ src/components/FrontPage/FrontPageContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FrontPageContent'
+});
+</script>
+
+<style scoped>
+.content {
+    height: 10px;
+    background-color: aqua;
+}
+</style>
